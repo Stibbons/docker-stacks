ddee49596a1dbc57cdc093898d068531b4583eee
Gaetan Semet <gaetan@xeberon.net>
2017-06-08 14:14:42 +0200

    [HACK] Force ownership change on /home/jovyan/work
    In case it is mounted through marathon, work is root, and
    the user is still 1000 (we don't want to change it).
    Just test is ownership is wront for this folder and restore
    it
    
    Signed-off-by: Gaetan Semet <gaetan@xeberon.net>

--- all-spark-notebook/start.sh
+++ all-spark-notebook/start.sh
@@ -5,6 +5,7 @@
 set -e
 
 # Handle special flags if we're root
+echo "UID=$UID"
 if [ $UID == 0 ] ; then
     # Change UID of NB_USER to NB_UID if it does not match
     if [ "$NB_UID" != $(id -u $NB_USER) ] ; then
